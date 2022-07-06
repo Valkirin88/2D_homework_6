7a95264948db8284697897e15c9b250667c0b780
Valkirin88 <91696211+Valkirin88@users.noreply.github.com>
2022-07-06 23:07:44 +0300

    v0.3

--- Assets/Scripts/GenerateLevel/GenerateLevelController.cs
+++ Assets/Scripts/GenerateLevel/GenerateLevelController.cs
@@ -1,21 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class GenerateLevelController 
+public class GenerateLevelController
 {
     private const int CountWall = 4;
-    
+
     private Tilemap _tileMapGround;
 
     private Tile _tileGround;
-        
+
     private int _widthMap;
-      
+
     private int _heightMap;
-        
+
     private int _factorSmooth;
 
     private int _randomFillPresent;
@@ -42,11 +39,94 @@ public class GenerateLevelController
     private void GenerateLevel()
     {
         RandomFillLevel();
+
+        for (var i = 0; i < _factorSmooth; i++)
+        {
+            SmoothMap();
+        }
+
+        DrawTilesOnMap();
     }
+
+
 
     private void RandomFillLevel()
     {
-        var psevdoRandom = new System.Random(); 
+        var psevdoRandom = new System.Random();
+
+        for (var x = 0; x < _widthMap; x++)
+        {
+            for (var y = 0; y < _heightMap; y++)
+            {
+                if (x == 0 || x == _widthMap - 1 || y == 0 || y == _heightMap - 1)
+                    _map[x, y] = 1;
+                else
+                    _map[x, y] = (psevdoRandom.Next(0, 100) < _randomFillPresent) ? 1 : 0;
+            }
+        }
 
     }
+
+    private void SmoothMap()
+    {
+        for (var x = 0; x < _widthMap; x++)
+        {
+            for (var y = 0; y < _heightMap; y++)
+            {
+                var neighbourWallTiles = GetSurroundingWallCount(x, y);
+                if (neighbourWallTiles > CountWall)
+                    _map[x, y] = 1;
+                else if (neighbourWallTiles < CountWall)
+                    _map[x, y] = 0;
+            }
+        }
+    }
+
+    private int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        var wallCount = 0;
+
+        for (var neighbourX = gridX - 1; neighbourX <= gridX +1 ; neighbourX++)
+        {
+            for (var neighbourY = gridY - 1; neighbourY <= gridY +1 ; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < _widthMap && neighbourY >= 0 && neighbourY < _heightMap)
+                {
+                    if (neighbourX !=gridX || neighbourY !=gridY)
+                        wallCount += _map[neighbourX, neighbourY];
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+        return wallCount;
+    }
+         
+
+
+    private void DrawTilesOnMap()
+    {
+        if (_map == null)
+            return;
+
+        for (var x = 0; x < _widthMap; x++)
+        {
+            for (var y = 0; y < _heightMap; y++)
+            {
+
+                if (_map[x, y] == 1)
+                {
+                    var positionTile = new Vector3Int(-_widthMap / 2 + x, -_heightMap / 2 + y, 0);
+                    _tileMapGround.SetTile(positionTile, _tileGround);
+                }
+            }
+        }
+    }
+
+
+
+
+
 }
---
+++ Assets/Scripts/GenerateLevel/Root.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Root : MonoBehaviour
+{
+    [SerializeField]
+    private GenerateLevelView _generateLevelView;
+
+    private GenerateLevelController _generateLevelController;
+
+    private void Awake()
+    {
+       _generateLevelController = new GenerateLevelController(_generateLevelView);
+        _generateLevelController.Awake();
+    }
+}
